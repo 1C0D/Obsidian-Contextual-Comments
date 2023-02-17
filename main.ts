31846dbe9a25a06d05b6cdaa24f7c48f6efea61c
1C0D <63018905+1C0D@users.noreply.github.com>
2023-02-17 05:53:48 +0100

    petite sauvegarde
    pour l'instant ça marche mais y a encore pas mal de choses à paufiner 
    puis idée retirer les commentaires 
    et enfin peut être une commande alternative pour les commentaires sur plusieurs lignes.

--- main.ts
+++ main.ts
@@ -1,137 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, EditorSelectionOrCaret, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class BetterComments extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: "better-comments",
+			name: "Better Comments",
+			editorCallback: (editor) => this.betterComments(editor),
+		});
+	}
+
+	betterComments = (editor: Editor): void => {
+		const selection = editor.getSelection();
+		const value = editor.getValue();
+		console.log("value", value);
+
+		// Check if the selected text is in a code block and detect its type
+		let codeBlockType = null;
+		//   const codeBlockRegex = /^```(js|javascript|python|jsx|ts|json|htmlmixed|xml|markdown|css|less|scss|sql|sh|console|cpp|clike|cs|java|go|ruby|lua|rust)\s([\s\S]*?)\n```$/gm;
+		const codeBlockRegex = /^```([a-z0-9-+]+)\n([\s\S]*?)\n```$/gm;
+
+		let codeBlockMatch;
+		const startOffset = editor.posToOffset(editor.getCursor("from"));
+		const endOffset = editor.posToOffset(editor.getCursor("to"));
+		const cursorIndex = Math.min(startOffset, endOffset);
+
+		while ((codeBlockMatch = codeBlockRegex.exec(value))) {
+			if (
+				codeBlockMatch.index <= cursorIndex &&
+				codeBlockMatch.index + codeBlockMatch[0].length >=
+					cursorIndex + selection.length
+			) {
+				codeBlockType = codeBlockMatch[1];
+				break;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// // Add comments to the selected text
+		let commentedSelection = "";
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// Define supported code block types
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		const cLikeTypes = [
+			"c",
+			"cpp",
+			"c++",
+			"clike",
+			"cs",
+			"java",
+			"js",
+			"javascript",
+			"ts",
+			"typescript",
+			"jsx",
+			"less",
+			"scss",
+			"",
+		];
+		const hashTypes = ["python", "ruby"];
+		const slashTypes = ["go", "javascript", "jsx", "typescript"];
+		const css = ["css"]; /**/
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+		if (codeBlockType && cLikeTypes.includes(codeBlockType.toLowerCase())) {
+			commentedSelection = selection.replace(/^(.*)$/gm, `// $1`);
+		} else if (
+			codeBlockType &&
+			hashTypes.includes(codeBlockType.toLowerCase())
+		) {
+			commentedSelection = selection.replace(/^(.*)$/gm, `# $1`);
+		} else if (
+			codeBlockType &&
+			slashTypes.includes(codeBlockType.toLowerCase())
+		) {
+			commentedSelection = selection.replace(/^(.*)$/gm, `// $1`);
+		} else if (codeBlockType && css.includes(codeBlockType.toLowerCase())) {
+			commentedSelection = selection.replace(/^(.*)$/gm, `/* $1 */`);
+		} else {
+			commentedSelection = selection.replace(/^(.*)$/gm, `%% $1 %%`);
+		}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+		// Replace the selected text with the commented text
+		editor.replaceSelection(commentedSelection);
+	};
 }
