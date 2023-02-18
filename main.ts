7ff85fba67c784b8c1bc44e28bfaf4b1dd962f58
1C0D <63018905+1C0D@users.noreply.github.com>
2023-02-18 01:02:10 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,62 @@
-import { Editor, EditorSelectionOrCaret, Plugin } from "obsidian";
+import { Editor, Plugin } from "obsidian";
+import { commentSelection } from "./CommentHelper";
 
-export default class BetterComments extends Plugin {
+export default class CodeBlocksComments extends Plugin {
 	async onload() {
 		this.addCommand({
 			id: "better-comments",
-			name: "Better Comments",
-			editorCallback: (editor) => this.betterComments(editor),
+			name: "Code Blocks Comments",
+			editorCallback: (editor) => this.codeBlocksComments(editor),
 		});
 	}
 
-	betterComments = (editor: Editor): void => {
+	codeBlocksComments = (editor: Editor): void => {
+		const { selection, value } = this.getSelectionAndValue(editor);
+
+		if (!selection) {
+			return;
+		}
+
+		const { line: anchor, ch: anchorChar } = editor.getCursor("from");
+		const { line: head, ch: headChar } = editor.getCursor("to");
+
+		const codeBlockType = this.getCodeBlockType(editor, value, selection);
+		const { commentedSelection, useSelection } = commentSelection({
+			selection,
+			codeBlockType,
+		});
+
+		editor.replaceSelection(commentedSelection);
+		const offset = commentedSelection.length - selection.length;
+		const lastLine = Math.max(head, anchor);
+		const firstLine = Math.min(head, anchor);
+		const firstLineChar = firstLine === anchor ? anchorChar : headChar;
+		const lastLineChar = lastLine === head ? headChar : anchorChar;
+		// console.log(
+		// 	`firstLine: ${firstLine}, 
+		// 	lastLine: ${lastLine}, 
+		// 	firstLineChar: ${firstLineChar}, 
+		// 	lastLineChar: ${lastLineChar}`
+		);
+		if (useSelection)
+			editor.setSelection(
+				{ line: firstLine, ch: firstLineChar },
+				{ line: lastLine, ch: lastLineChar + offset / 2 } //bout de ligne?
+			);
+	};
+
+	getSelectionAndValue = (editor: Editor) => {
 		const selection = editor.getSelection();
 		const value = editor.getValue();
-		console.log("value", value);
+		return { selection, value };
+	};
 
-		// Check if the selected text is in a code block and detect its type
-		let codeBlockType = null;
-		//   const codeBlockRegex = /^```(js|javascript|python|jsx|ts|json|htmlmixed|xml|markdown|css|less|scss|sql|sh|console|cpp|clike|cs|java|go|ruby|lua|rust)\s([\s\S]*?)\n```$/gm;
+	getCodeBlockType = (
+		editor: Editor,
+		value: string,
+		selection: string
+	): string | null => {
 		const codeBlockRegex = /^```([a-z0-9-+]+)\n([\s\S]*?)\n```$/gm;
-
 		let codeBlockMatch;
 		const startOffset = editor.posToOffset(editor.getCursor("from"));
 		const endOffset = editor.posToOffset(editor.getCursor("to"));
@@ -30,55 +68,9 @@ export default class BetterComments extends Plugin {
 				codeBlockMatch.index + codeBlockMatch[0].length >=
 					cursorIndex + selection.length
 			) {
-				codeBlockType = codeBlockMatch[1];
-				break;
+				return codeBlockMatch[1];
 			}
 		}
-
-		// // Add comments to the selected text
-		let commentedSelection = "";
-
-		// Define supported code block types
-
-		const cLikeTypes = [
-			"c",
-			"cpp",
-			"c++",
-			"clike",
-			"cs",
-			"java",
-			"js",
-			"javascript",
-			"ts",
-			"typescript",
-			"jsx",
-			"less",
-			"scss",
-			"",
-		];
-		const hashTypes = ["python", "ruby"];
-		const slashTypes = ["go", "javascript", "jsx", "typescript"];
-		const css = ["css"]; /**/
-
-		if (codeBlockType && cLikeTypes.includes(codeBlockType.toLowerCase())) {
-			commentedSelection = selection.replace(/^(.*)$/gm, `// $1`);
-		} else if (
-			codeBlockType &&
-			hashTypes.includes(codeBlockType.toLowerCase())
-		) {
-			commentedSelection = selection.replace(/^(.*)$/gm, `# $1`);
-		} else if (
-			codeBlockType &&
-			slashTypes.includes(codeBlockType.toLowerCase())
-		) {
-			commentedSelection = selection.replace(/^(.*)$/gm, `// $1`);
-		} else if (codeBlockType && css.includes(codeBlockType.toLowerCase())) {
-			commentedSelection = selection.replace(/^(.*)$/gm, `/* $1 */`);
-		} else {
-			commentedSelection = selection.replace(/^(.*)$/gm, `%% $1 %%`);
-		}
-
-		// Replace the selected text with the commented text
-		editor.replaceSelection(commentedSelection);
+		return null;
 	};
 }
