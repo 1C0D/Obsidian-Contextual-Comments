13fe8e7b27c67f15c1e02f26677e967b0515de5d
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 01:54:04 +0100

    piou

--- _autogit.bat
+++ _autogit.bat
@@ -10,7 +10,7 @@ rem Commit the changes with the specified commit message
 git commit -m "%commitMessage%"
 
 rem Display the status of the Git repository
-git status
+REM git status
 
 rem Pause the console to keep it open
 pause
