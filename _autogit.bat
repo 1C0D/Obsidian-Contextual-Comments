3cdf7278a801e689d545a1a829a88cc16523a255
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 02:00:04 +0100

    piou

--- _autogit.bat
+++ _autogit.bat
@@ -9,8 +9,17 @@ git add .
 rem Commit the changes with the specified commit message
 git commit -m "%commitMessage%"
 
+rem Push the changes to the remote repository
+REM git push
+
 rem Display the status of the Git repository
 git status
 
+rem Open the GitHub repository page
+set remoteUrl=
+for /f "tokens=2" %%a in ('git remote get-url origin') do set remoteUrl=%%a
+if not "%remoteUrl%" == "" start "" "%remoteUrl%"
+
 rem Pause the console to keep it open
 pause
+
