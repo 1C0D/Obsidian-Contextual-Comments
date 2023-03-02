30e487aee5700bd2d828099f0282151698ab3c0f
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 03:48:27 +0100

    piou

--- _autogit.bat
+++ _autogit.bat
@@ -20,8 +20,7 @@ REM modify version.json
 REM exe update-version.ps1
 powershell.exe -ExecutionPolicy Bypass -File "%~dp0_version_.ps1" %new_version%
 
-REM del the file created by the prompt // mettre en attente
-REM del your
+
 
 rem Prompt the user for a commit message
 set /p commitMessage="Enter commit message: "
@@ -39,18 +38,13 @@ REM open your repo
 for /f "usebackq" %%G in (`git config --get remote.origin.url`) do set remoteUrl=%%G
 start "" "%remoteUrl%"
 
+REM del the file created by the prompt // mettre en attente
+del your
 
 rem Pause the console to keep it open
 pause
 
-REM giturl=$(git config --get remote.origin.url) 
-REM giturl=${giturl/git\@github\.com\:/https://github.com/}
-REM giturl=${giturl/\.git/\/tree/}
-REM branch="$(git symbolic-ref HEAD 2>/dev/null)" ||
-REM branch="(unnamed branch)"     # detached HEAD
-REM branch=${branch##refs/heads/}
-REM giturl=$giturl/tree/$branch
-REM start $giturl
+
 
 
 
