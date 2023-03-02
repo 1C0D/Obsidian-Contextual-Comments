3a2f6c69212814c8a3bfab63d7bd1dece2c0d661
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 03:46:35 +0100

    piou

--- _autogit.bat
+++ _autogit.bat
@@ -1,5 +1,28 @@
 @echo off
 
+REM prompt to enter your new version
+set /p new_version="Enter new version number: "
+set "versionMinObsidian=0.15.0"
+
+REM modify version.json
+(
+    echo {
+    echo     "%new_version%": "%versionMinObsidian%"
+    echo }
+) > versions.json
+
+(
+    echo {
+    echo     "%new_version%": "%versionMinObsidian%"
+    echo }
+) > versions.json
+
+REM exe update-version.ps1
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0_version_.ps1" %new_version%
+
+REM del the file created by the prompt // mettre en attente
+REM del your
+
 rem Prompt the user for a commit message
 set /p commitMessage="Enter commit message: "
 
@@ -8,22 +31,14 @@ git add .
 
 rem Commit the changes with the specified commit message
 git commit -m "%commitMessage%"
-
+REM git push
 rem Display the status of the Git repository
 git status
 
-
+REM open your repo
 for /f "usebackq" %%G in (`git config --get remote.origin.url`) do set remoteUrl=%%G
-REM set remoteUrl=!remoteUrl:.git=!
 start "" "%remoteUrl%"
-REM https://github.com/https://github.com/1C0D/Obsidian-Contextual-Comments.git
 
-REM git remote -v
-REM for /f "tokens=2" %%a in ('git config --get remote.origin.url') do (
-  REM set giturl=%%a
-  REM set giturl=!giturl/git\@github\.com\:/https://github.com/!
-  REM start "" "!giturl!"
-REM )
 
 rem Pause the console to keep it open
 pause
