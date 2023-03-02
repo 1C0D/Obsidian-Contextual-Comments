f8b424a5856c30a6ab7c64985c46c7f9b5171721
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 01:52:36 +0100

    piou

---
+++ _autogit.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Prompt the user for a commit message
+set /p commitMessage="Enter commit message: "
+
+rem Add all modified files to the Git staging area
+git add .
+
+rem Commit the changes with the specified commit message
+git commit -m "%commitMessage%"
+
+rem Display the status of the Git repository
+git status
+
+rem Pause the console to keep it open
+pause
