05aa4dc3e32c6907337e20f588023f55b79ba719
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-02 02:14:53 +0100

    aa

--- _autogit.bat
+++ _autogit.bat
@@ -9,21 +9,10 @@ git add .
 rem Commit the changes with the specified commit message
 git commit -m "%commitMessage%"
 
-rem Push the changes to the remote repository
-REM git push
-
 rem Display the status of the Git repository
 git status
 
-rem Display all environment variables, including remoteUrl
-set
-
-rem Open the GitHub repository page
-set remoteUrl=
-for /f "tokens=2" %%a in ('git remote get-url origin') do set remoteUrl=%%a
-if not "%remoteUrl%" == "" start "" "%remoteUrl%"
+git remote -v
 
 rem Pause the console to keep it open
 pause
-
-
