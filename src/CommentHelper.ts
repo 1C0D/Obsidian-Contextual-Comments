9fc5fab26c4b540d699194f74c3b539a3d8db795
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-23 23:28:12 +0100

    fixed overwritten code

--- src/CommentHelper.ts
+++ src/CommentHelper.ts
@@ -16,11 +16,24 @@ function commentType(codeBlockType: string) {
 		"jsx",
 		"less",
 		"scss",
+		"jsonc",
+		"dataviewjs",
 		"empty",
 	];
-	const hashTypes = ["python", "py", "ruby", "bash", "ps1"];
+	const hashTypes = [
+		"python",
+		"py",
+		"ruby",
+		"bash",
+		"ps1",
+		"zsh",
+		"sh",
+		"applescript",
+		"yaml",
+		"yml",
+	];
 	const lua = ["lua", "sql"];
-	const html = ["html", "xml"];
+	const html = ["html", "xml", "md"];
 	const css = ["css"];
 	const templater = ["templater"];
 	const bat = ["bat"];
@@ -54,27 +67,34 @@ export function commentSelection(
 	if (codeBlockType) {
 		const varName = commentType(codeBlockType);
 		if (varName === "cLikeTypes" || varName === "templater") {
-			console.log("ici")
 			const pattern = /^(\s*)\/\/\s?(.*)$/gm;
 			if (pattern.test(selection)) {
 				commentedSelection = selection.replace(pattern, `$1$2`);
 			} else {
-				console.log("là")
-				commentedSelection = selection.replace(/^(\s*)(.*)$/gm, `$1// $2`);
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1// $2`
+				);
 			}
 		} else if (varName === "hashTypes") {
 			const pattern = /^(\s*)#\s?(.*)$/gm;
 			if (pattern.test(selection)) {
 				commentedSelection = selection.replace(pattern, `$1$2`);
 			} else {
-				commentedSelection = selection.replace(/^(\s*)(.*)$/gm, `$1# $2`);
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1# $2`
+				);
 			}
 		} else if (varName === "lua") {
 			const pattern = /^(\s*)--\s?(.*)$/gm;
 			if (pattern.test(selection)) {
 				commentedSelection = selection.replace(pattern, `$1$2`);
 			} else {
-				commentedSelection = selection.replace(/^(\s*)(.*)$/gm, `$1-- $2`);
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1-- $2`
+				);
 			}
 		} else if (varName === "bat") {
 			const pattern = /^[Rr][Ee][Mm]\s?(.*)$/gm;
@@ -86,7 +106,6 @@ export function commentSelection(
 		} else if (varName === "css") {
 			const pattern = /^\/\*\s?(.*)\s?\*\/$/gms;
 			if (pattern.test(selection)) {
-				console.log("ici");
 				commentedSelection = selection.replace(pattern, `$1`);
 			} else {
 				commentedSelection = selection.replace(/^(.*)$/gms, `/* $1 */`);
